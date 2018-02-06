4c09dd98e75e3d7142d8ff2270f6bad6ba7145ba
zhaoning <m13298317376@163.com>
2018-02-06 18:01:45 +0800

    样式修改

---
+++ 微信小程序项目10/pages/money-list/money-list.js
@@ -0,0 +1,74 @@
+Page({
+    data: {
+        hasMore: true,
+        order: [{
+            src: "",
+            merchantName: "良品铺子",
+            date: "2018-01-24 10：22",
+            money: 300
+        }, {
+            src: "",
+            merchantName: "良品铺子1",
+            date: "2018-01-24 10：22",
+            money: -300
+        }]
+    },
+
+    onLaunch: function (e) {
+        var that = this;
+        var url = '';
+        wx.request({
+            url: '',
+            data: "",
+            method: 'POST',  //必须大写哦
+            success: function (json) {
+                that.setData({
+                    list: that.data.order.concat(json.data.result.list),
+                });
+            },
+            fail: function (json) {
+                alert(json.desc)
+            },
+            complete: function (json) {
+                wx.hideToast();
+                alert(json.desc)
+            }
+        });
+
+    },
+    logbtn: function () {
+        wx.showToast({
+            title: '登陆成功',
+            icon: 'success',
+            duration: 1200
+        })
+    },
+    loadMore: function (e) {
+        var that = this;
+        wx.showToast({
+            title: '加载中。。。',
+            icon: 'loading'
+        });
+        if (that.data.hasMore) {
+            wx.request({
+                url: '',
+                data: "",
+                method: 'POST',  //必须大写哦
+                success: function (json) {
+                    that.setData({
+                        list: that.data.order.concat(json.data.result.list)
+                    });
+                },
+                fail: function (json) {
+                    alert(json.desc);
+                },
+                complete: function (json) {
+                    wx.hideToast();
+                    alert(json.desc);
+                }
+            });
+        }
+
+    }
+
+})
