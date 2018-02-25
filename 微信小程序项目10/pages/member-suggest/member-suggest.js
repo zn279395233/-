4211faeedd2fcb981dbd5eddaec0d370562f7121
xiehao <1171990125@qq.com>
2018-02-25 14:56:43 +0800

    下

--- 微信小程序项目10/pages/member-suggest/member-suggest.js
+++ 微信小程序项目10/pages/member-suggest/member-suggest.js
@@ -1,11 +1,19 @@
 // pages/member-suggest/member-suggest.js
+var myreg = /^1([358][0-9]|4[579]|66|7[0135678]|9[89])[0-9]{8}$/;
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    types:0
+    types:0,
+    popErrorMsg: "",
+    _num: 1,
+    content:"",
+    mobile:"",
+    noteMaxLen: 200,
+    noteNowLen: 0
+
   },
 
   /**
@@ -14,16 +22,51 @@ Page({
   onLoad: function (options) {
   
   },
+  // 手机号
+  amountInput:function(e){
+    var that =this;
+    that.setData({
+      mobile: e.detail.value
+    })
+  },
   // 单选按钮选中切换
-  radioChange:function(e){
+  radioChange:function (e){
     var that = this;
     that.setData({
       types:e.detail.value
     })
   },
   // 提交
-  saveBtn:function(){
+  saveBtn:function (){
+    console.log(this.data.mobile)
+    if (!(myreg.test(this.data.mobile))){
+      this.setData(
+        { _num: 2, popErrorMsg: "手机号错误" },
+      );
+      setTimeout(() => {
+        this.setData(
+          { _num: 1, },
+        );
 
+      }, 1500);
+    }
+    return false
+  },
+  bindTextAreaChange:function (e) {
+    var value = e.detail.value, len = parseInt(value.length);
+    this.setData({noteNowLen: len,})
+    if (len>=200) {
+      this.setData(
+        { _num: 2, popErrorMsg: "" },
+      );
+      setTimeout(() => {
+        this.setData(
+          { _num: 1, },
+        );
+
+      }, 1500);
+    }
+   
   },
 
   /**
